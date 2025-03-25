061d9e99ec88bc5c035ec206e67d54bb5a4cbc05
billalcodes <bilal.105.ahmed@gmail.com>
2025-03-25 14:55:03 +0500

    rwer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,19 @@ export default function RootLayout({
           rel="icon"
           href="/images/logo/logo.png"
         />
+
+  <script dangerouslySetInnerHTML={{ __html: `
+    (function(w,d,s,pc){
+      pc.projectId = "lawfirm-f90875a5-1a28-4d07-b19e-459ca41a8276";
+      w.PrecisionConversionsConfig = pc;
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s);
+      j.async=true;
+      j.src="https://lh8llf3eu0.execute-api.us-east-1.amazonaws.com/v2/script/" + pc.projectId;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,"script",{});
+  ` }} />
+
       </head>
       <body suppressHydrationWarning={true}>
         {loading ? <Loader /> : children}

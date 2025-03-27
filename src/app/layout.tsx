aaaabc792a4e3872b194d89cf29a10f54c058335
billalcodes <bilal.105.ahmed@gmail.com>
2025-03-27 15:45:08 +0500

    tes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 <head>
   <script dangerouslySetInnerHTML={{ __html: `
     (function(w,d,s,pc){
-      pc.projectId = "lawfirm-46234f7a-13ba-4cdf-8510-f5d7d2cabda4";
+      pc.projectId = "lawfirm-db0928fe-52e2-475b-a29c-2553a3924bb3";
       w.PrecisionConversionsConfig = pc;
       var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s);
